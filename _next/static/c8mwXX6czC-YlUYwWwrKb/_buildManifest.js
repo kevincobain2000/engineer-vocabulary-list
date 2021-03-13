54bd17cbd05a4d967c923067906dd720e50f5089
kevincobain2000 <kevincobain2000@users.noreply.github.com>
2021-03-13 05:47:22 +0000

    deploy: 5be8d841f8d8f151c419d6b901441c3a718a6b23

---
+++ _next/static/c8mwXX6czC-YlUYwWwrKb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-acad7a774efde93fc1bb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1b0d622627bd50f0172e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
